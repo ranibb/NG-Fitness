c9520599c64015156600897d5179389918f8673d
Rani Abu Sultan <raniabusultan@gmail.com>
2018-10-04 11:55:53 +0300

    Adding a Cancel Dialog Screen

--- src/app/training/current-training/current-training.component.ts
+++ src/app/training/current-training/current-training.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material';
+import { StopTrainingComponent } from './stop-training.component';
 
 @Component({
   selector: 'app-current-training',
@@ -7,9 +9,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CurrentTrainingComponent implements OnInit {
   progress = 0;
-  timer: number;
+  timer: ReturnType<typeof setTimeout>;
 
-  constructor() { }
+  constructor(private dialog: MatDialog) { }
 
   ngOnInit() {
     this.timer = setInterval(() => {
@@ -21,6 +23,7 @@ export class CurrentTrainingComponent implements OnInit {
   }
 
   onStop() {
-    clearInterval(this.timer)
+    clearInterval(this.timer);
+    this.dialog.open(StopTrainingComponent)
   }
 }
