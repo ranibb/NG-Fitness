ab44d5fd0be722a7db2a267df9924e7b5e4d3451
Rani Abu Sultan <raniabusultan@gmail.com>
2018-10-06 21:24:42 +0300

    Adding Pagination to the Data Table

--- src/app/training/past-trainings/past-trainings.component.ts
+++ src/app/training/past-trainings/past-trainings.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
-import { MatTableDataSource, MatSort } from '@angular/material';
+import { MatTableDataSource, MatSort, MatPaginator } from '@angular/material';
 
 import { Exercise } from '../exercise.model'
 import { TrainingService } from '../training.service';
@@ -17,6 +17,9 @@ export class PastTrainingsComponent implements OnInit, AfterViewInit {
   @ViewChild(MatSort)
   sort: MatSort;
 
+  @ViewChild(MatPaginator)
+  paginator: MatPaginator;
+
   constructor(private trainingService: TrainingService) { }
 
   ngOnInit() {
@@ -25,6 +28,7 @@ export class PastTrainingsComponent implements OnInit, AfterViewInit {
 
   ngAfterViewInit() {
     this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
   }
 
   doFilter(filterValue: string) {
