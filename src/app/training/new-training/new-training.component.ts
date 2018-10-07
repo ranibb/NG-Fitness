0ac53b76e6df6022a17d7a348be18c6f2c0b99b7
Rani Abu Sultan <raniabusultan@gmail.com>
2018-10-07 14:40:27 +0300

    Listening to Snapshot Changes (of Firestore, incl. Metadata)

--- src/app/training/new-training/new-training.component.ts
+++ src/app/training/new-training/new-training.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { AngularFirestore } from '@angular/fire/firestore';
-import { Observable } from 'rxjs'
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators'
 
 import { TrainingService } from '../training.service';
 import { Exercise } from '../exercise.model';
@@ -14,15 +15,48 @@ import { Exercise } from '../exercise.model';
 })
 export class NewTrainingComponent implements OnInit {
 
-  exercises: Observable<any>;
+  exercises: Observable<Exercise[]>;
 
   constructor(private trainingService: TrainingService, private db: AngularFirestore) { }
 
   ngOnInit() {
-    this.exercises = this.db.collection('availableExercises').valueChanges();
+
+    /* Method 1, access the data in the document but without access to metadata such as the doc ID */
+    // this.exercises = this.db.collection('availableExercises').valueChanges();
     // this.db.collection('availableExercises').valueChanges().subscribe(result => {
     //   console.log(result);
     // })
+
+    /* Method 2, access the data in the document and also get metadata such as the doc ID */
+    this.exercises = this.db
+      .collection('availableExercises')
+      .snapshotChanges()
+      .pipe(
+        map(docArray => {
+          return docArray.map(doc => {
+            return {
+              id: doc.payload.doc.id,
+              ...doc.payload.doc.data() as Exercise
+            }
+          })
+        })
+      )    
+    // this.db
+    //   .collection('availableExercises')
+    //   .snapshotChanges()
+    //   .pipe(
+    //     map(docArray => {
+    //       return docArray.map(doc => {
+    //         return {
+    //           id: doc.payload.doc.id,
+    //           ...doc.payload.doc.data()
+    //         }
+    //       })
+    //     })
+    //   )
+    //   .subscribe(result => {
+    //     console.log(result);
+    //   })
   }
 
   onStartTraining(form: NgForm) {
