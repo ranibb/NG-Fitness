fdc9c869373ec83f734e8dc84797b189da56db6d
Rani Abu Sultan <raniabusultan@gmail.com>
2018-10-08 15:28:27 +0300

    Splitting Up Routes - creating training routing module (loading it eagerly)

--- src/app/training/training.module.ts
+++ src/app/training/training.module.ts
@@ -7,6 +7,7 @@ import { NewTrainingComponent } from "./new-training/new-training.component";
 import { PastTrainingsComponent } from "./past-trainings/past-trainings.component";
 import { StopTrainingComponent } from "./current-training/stop-training.component";
 import { SharedModule } from "../shared/shared.module";
+import { TrainingRoutingModule } from "./training-routing.module";
 
 @NgModule({
     declarations:[
@@ -19,6 +20,7 @@ import { SharedModule } from "../shared/shared.module";
     imports: [
         SharedModule,
         AngularFirestoreModule,
+        TrainingRoutingModule
     ],
     exports: [],
     entryComponents: [StopTrainingComponent]
