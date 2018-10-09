fc9afed5ec8976f48eecc41c57f6fa47abaf3777
Rani Abu Sultan <raniabusultan@gmail.com>
2018-10-09 20:25:05 +0300

    NgRx example

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -2,11 +2,13 @@ import { Injectable } from "@angular/core";
 import { Router } from "@angular/router";
 import { Subject } from 'rxjs';
 import { AngularFireAuth } from '@angular/fire/auth';
+import { Store } from "@ngrx/store";
 
 import { User } from "./user.model";
 import { AuthData } from "./auth-data.model";
 import { TrainingService } from "../training/training.service";
 import { UIService } from "../shared/ui.service";
+import * as fromApp from '../app.reducer';
 
 
 @Injectable()
@@ -18,7 +20,8 @@ export class AuthService {
     private router: Router,
     private afAuth: AngularFireAuth,
     private trainingService: TrainingService,
-    private uiService: UIService
+    private uiService: UIService,
+    private store: Store<{ui: fromApp.State}>
   ) { }
 
   initAuthListener() {
@@ -37,29 +40,35 @@ export class AuthService {
   }
 
   registerUser(authData: AuthData) {
-    this.uiService.loadingStateChanged.next(true);
+    // this.uiService.loadingStateChanged.next(true);
+    this.store.dispatch({ type: 'START_LOADING' })
     this.afAuth.auth.createUserAndRetrieveDataWithEmailAndPassword(
       authData.email,
       authData.password
     ).then(result => {
       this.uiService.loadingStateChanged.next(false);
+      this.store.dispatch({ type: 'STOP_LOADING' })
     })
       .catch(error => {
-        this.uiService.loadingStateChanged.next(false);
+        // this.uiService.loadingStateChanged.next(false);
+        this.store.dispatch({ type: 'STOP_LOADING' })
         this.uiService.showSnackbar(error.message, null, 3000);
       })
   }
 
   login(authData: AuthData) {
-    this.uiService.loadingStateChanged.next(true);
+    // this.uiService.loadingStateChanged.next(true);
+    this.store.dispatch({ type: 'START_LOADING' })
     this.afAuth.auth.signInWithEmailAndPassword(
       authData.email,
       authData.password
     ).then(result => {
-      this.uiService.loadingStateChanged.next(false);
+      // this.uiService.loadingStateChanged.next(false);
+      this.store.dispatch({ type: 'STOP_LOADING' })
     })
       .catch(error => {
-        this.uiService.loadingStateChanged.next(false);
+        // this.uiService.loadingStateChanged.next(false);
+        this.store.dispatch({ type: 'STOP_LOADING' })
         this.uiService.showSnackbar(error.message, null, 3000);
       })
   }
--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,45 +1,54 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
-import { Subscription } from 'rxjs'
+import { Subscription, Observable } from 'rxjs'
+import { map } from 'rxjs/operators'
+import { Store } from '@ngrx/store';
 
 import { AuthService } from '../auth.service';
 import { UIService } from '../../shared/ui.service';
+import * as fromApp from '../../app.reducer';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit, OnDestroy {
+export class LoginComponent implements OnInit {
 
-  isLoading = false;
+  isLoading$: Observable<boolean>;
   private loadingSubs: Subscription;
 
-  constructor(private fb: FormBuilder, private authService: AuthService, private uiService: UIService) { }
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private uiService: UIService,
+    private store: Store<{ ui: fromApp.State }>
+  ) { }
 
   ngOnInit() {
-    this.loadingSubs = this.uiService.loadingStateChanged.subscribe(isLoading => {
-      this.isLoading = isLoading
-    })
-  }
-
-  loginForm = this.fb.group({
-    email: ['', Validators.compose([Validators.required,Validators.email])],
-    password: ['', Validators.required]
-  });
-
-  onSubmit(loginForm) {
-    console.log(loginForm);
-    this.authService.login({
-      email: this.loginForm.value.email,
-      password: this.loginForm.value.password
-    })
-  }
-
-  ngOnDestroy() {
-    if (this.loadingSubs) {
-      this.loadingSubs.unsubscribe();
+    this.isLoading$ = this.store.pipe(map(state => state.ui.isLoading))
+      // this.loadingSubs = this.uiService.loadingStateChanged.subscribe(isLoading => {
+      //   this.isLoading = isLoading
+      // })
     }
-  }
 
-}
+    loginForm = this.fb.group({
+      email: ['', Validators.compose([Validators.required, Validators.email])],
+      password: ['', Validators.required]
+    });
+
+    onSubmit(loginForm) {
+      console.log(loginForm);
+      this.authService.login({
+        email: this.loginForm.value.email,
+        password: this.loginForm.value.password
+      })
+    }
+
+    // ngOnDestroy() {
+    //   if (this.loadingSubs) {
+    //     this.loadingSubs.unsubscribe();
+    //   }
+    // }
+
+  }
