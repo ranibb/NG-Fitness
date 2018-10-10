494892024616ced0a2b5c69b4cd6dba2c003c622
Rani Abu Sultan <raniabusultan@gmail.com>
2018-10-10 21:38:20 +0300

    fixed one logical error

--- src/app/training/exercise.model.ts
+++ src/app/training/exercise.model.ts
@@ -4,5 +4,5 @@ export interface Exercise {
     duration: number;
     calories: number;
     date?: Date;
-    state?: 'completed' | 'cancalled' | null;
+    state?: 'completed' | 'cancelled' | null;
 }
